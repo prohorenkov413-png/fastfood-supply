9c4efe83452a05c031fb6722d8f05d05d8827df3
longust <prohorenkov413@gmail.com>
2026-05-15 22:08:18 +0300

    Добавлены пакеты для PostgreSQL и переменные окружения

--- fastfood_project/settings.py
+++ fastfood_project/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -73,12 +74,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'fastfood_project.wsgi.application'
 
-# БАЗА ДАННЫХ
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 # ВАЛИДАЦИЯ ПАРОЛЕЙ
